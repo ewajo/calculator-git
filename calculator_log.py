f35112cdafa78248fd6ab738caa3f3ebb61b4daa
Ewa Sawicka <ewajoanna076@gmail.com>
2023-04-17 22:32:28 +0200

    correct1

--- calculator_log.py
+++ calculator_log.py
@@ -3,30 +3,42 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+     
+def add(x,y):
+    logging.info(f"Dodaję {x} i {y}")
+    return x + y
+
+def subtract(x,y):
+    logging.info(f"Odejmuję {x} i {y}")
+    return x - y
+
+def multiply(x,y):
+    logging.info(f"Mnożę {x} i {y}")
+    return x * y 
+
+def divide(x,y):
+    logging.info(f"Dzielę {x} i {y}")
+    return x / y 
+
 def calculate (operator,x,y):
     if operator == "1":
-        logging.info("Dodaję", x, "i",y)
-        return x + y
-    elif operator == "2": 
-        logging.info("Odejmuję", x ,"i", y)
-        return x - y
-    elif operator == "3": 
-        logging.info("Mnożę", x,"i",y)
-        return x * y 
-    elif operator == "4": 
-        logging.info("Dzielę", x,"i", y)
-        return x / y 
-    else: return None
+        result=add(x,y)
+    elif operator == "2":
+        result=subtract(x,y)
+    elif operator == "3":
+        result=multiply(x,y)
+    elif operator == "4":
+        result=divide(x,y)
+    return result
 
 if __name__ == "__main__":
-        operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: 1")
-        x = float(input("Podaj składnik 1:"))
-        y = float(input("Podaj składnik 2:"))
-        operator = sys.argv[1:]
-        result = calculate(operation, x, y)
-        print(result)
-if y==0:
-    print("Nie można dzielić przez zero")
-elif result is None:
-    print("Niedozwolona operacja")
+    operator = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie ")
+    x = float(input("Podaj składnik 1:"))
+    y = float(input("Podaj składnik 2:"))
+    result = calculate(operator, x,y)
+    print(result)
+    if y==0:
+        print("Nie można dzielić przez zero")
+    elif result is None:
+        print("Niedozwolona operacja")
         
